2ffd541bfe870c331d4a6ce948226c9d94b1ccce
IvanAvila123 <ivanavilar456@gmail.com>
2023-10-30 14:57:41 -0600

    se quita el buscador y las notificaciones

--- templates/header-user.php
+++ templates/header-user.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['user_id'])) {
 $user_id = $_SESSION['user_id'];
 
 $sql = "SELECT * FROM users WHERE id = $user_id";
-$query = mysqli_query($con,$sql);
+$query = mysqli_query($con, $sql);
 
 // Comprueba si la consulta fue exitosa
 if (!$query) {
@@ -31,261 +31,150 @@ $user = mysqli_fetch_assoc($query);
 ?>
 
 <!DOCTYPE html>
-    <html lang="en">
+<html lang="en">
 
-    <head>
-        <meta charset="utf-8">
-        <title>Costumer-wolftek</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description">
-        <meta content="Coderthemes" name="author">
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/icono-logo.png">
+<head>
+    <meta charset="utf-8">
+    <title>Costumer-wolftek</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description">
+    <meta content="Coderthemes" name="author">
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/icono-logo.png">
 
-        <!-- third party css -->
-        <link href="assets/css/vendor/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css">
-        <link href="assets/DataTable/datatables.min.css" rel="stylesheet">
-        <!-- third party css end -->
+    <!-- third party css -->
+    <link href="assets/css/vendor/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css">
+    <link href="assets/DataTable/datatables.min.css" rel="stylesheet">
+    <!-- third party css end -->
 
-        <!-- App css -->
-        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css">
-        <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" id="light-style">
-        <link href="assets/css/app-dark.min.css" rel="stylesheet" type="text/css" id="dark-style">
+    <!-- App css -->
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" id="light-style">
+    <link href="assets/css/app-dark.min.css" rel="stylesheet" type="text/css" id="dark-style">
 
-    </head>
+</head>
 
-    <body class="loading" data-layout-config='{"leftSideBarTheme":"dark","layoutBoxed":false, "leftSidebarCondensed":false, "leftSidebarScrollable":false,"darkMode":false, "showRightSidebarOnStart": true}'>
-        <!-- Begin page -->
-        <div class="wrapper">
-            <!-- ========== Left Sidebar Start ========== -->
-            <div class="leftside-menu">
-    
-                <!-- LOGO -->
-                <a href="index.html" class="logo text-center logo-light">
-                    <span class="logo-lg">
-                        <img src="assets/images/Wolftek.png" alt="" height="50">
-                    </span>
-                    <span class="logo-sm">
-                        <img src="assets/images/logo-sm.png" alt="" height="40">
-                    </span>
-                </a>
+<body class="loading" data-layout-config='{"leftSideBarTheme":"dark","layoutBoxed":false, "leftSidebarCondensed":false, "leftSidebarScrollable":false,"darkMode":false, "showRightSidebarOnStart": true}'>
+    <!-- Begin page -->
+    <div class="wrapper">
+        <!-- ========== Left Sidebar Start ========== -->
+        <div class="leftside-menu">
 
-                <!-- LOGO -->
-                <a href="index.html" class="logo text-center logo-dark">
-                    <span class="logo-lg">
-                        <img src="assets/images/Wolftek-light.png" alt="" height="50">
-                    </span>
-                    <span class="logo-sm">
-                        <img src="assets/images/logo-light.png" alt="" height="40">
-                    </span>
-                </a>
-    
-                <div class="h-100" id="leftside-menu-container" data-simplebar="">
+            <!-- LOGO -->
+            <a href="index.html" class="logo text-center logo-light">
+                <span class="logo-lg">
+                    <img src="assets/images/Wolftek.png" alt="" height="50">
+                </span>
+                <span class="logo-sm">
+                    <img src="assets/images/logo-sm.png" alt="" height="40">
+                </span>
+            </a>
 
-                    <!--- Sidemenu -->
-                    <ul class="side-nav">
+            <!-- LOGO -->
+            <a href="index.html" class="logo text-center logo-dark">
+                <span class="logo-lg">
+                    <img src="assets/images/Wolftek-light.png" alt="" height="50">
+                </span>
+                <span class="logo-sm">
+                    <img src="assets/images/logo-light.png" alt="" height="40">
+                </span>
+            </a>
 
-                        <li class="side-nav-title side-nav-item">Navegacion</li>
+            <div class="h-100" id="leftside-menu-container" data-simplebar="">
 
-                        <li class="side-nav-item">
-                            <a data-bs-toggle="collapse" href="#sidebarDashboards" aria-expanded="false" aria-controls="sidebarDashboards" class="side-nav-link">
-                                <i class="uil-home-alt"></i>
-                                <span class="badge bg-success float-end">4</span>
-                                <span> Dashboards </span>
+                <!--- Sidemenu -->
+                <ul class="side-nav">
+
+                    <li class="side-nav-title side-nav-item">Navegacion</li>
+
+                    <li class="side-nav-item">
+                        <a data-bs-toggle="collapse" href="#sidebarDashboards" aria-expanded="false" aria-controls="sidebarDashboards" class="side-nav-link">
+                            <i class="uil-home-alt"></i>
+                            <span class="badge bg-success float-end">4</span>
+                            <span> Dashboards </span>
+                        </a>
+                        <div class="collapse" id="sidebarDashboards">
+                            <ul class="side-nav-second-level">
+                                <li>
+                                    <a href="Usuario.php">Clientes</a>
+                                </li>
+                                <li>
+                                    <a href="ventasuser.php">Ventas/Oportuniddes</a>
+                                </li>
+                                <li>
+                                    <a href="informesUser.php">Informes</a>
+                                </li>
+
+                                <li>
+                                    <a href="renovacionesUser.php">Renovaciones/Renovaciones Anticipadas</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+
+                    <li class="side-nav-title side-nav-item">Apps</li>
+
+                    <li class="side-nav-item">
+                        <a href="apps-chat.html" class="side-nav-link">
+                            <i class="uil-comments-alt"></i>
+                            <span> Chat (Promixamente) </span>
+                        </a>
+                    </li>
+
+                    <li class="side-nav-item">
+                        <a href="apps-file-manager.html" class="side-nav-link">
+                            <i class="uil-folder-plus"></i>
+                            <span> File Manager (Proximamente) </span>
+                        </a>
+                    </li>
+
+                </ul>
+
+                <div class="clearfix"></div>
+
+            </div>
+            <!-- Sidebar -left -->
+
+        </div>
+
+        <div class="content-page">
+            <div class="content">
+                <!-- Topbar Start -->
+                <div class="navbar-custom">
+                    <ul class="list-unstyled topbar-menu float-end mb-0">
+
+                        <li class="notification-list">
+                            <a class="nav-link end-bar-toggle" href="javascript: void(0);">
+                                <i class="dripicons-gear noti-icon"></i>
                             </a>
-                            <div class="collapse" id="sidebarDashboards">
-                                <ul class="side-nav-second-level">
-                                    <li>
-                                        <a href="Usuario.php">Clientes</a>
-                                    </li>
-                                    <li>
-                                        <a href="ventasuser.php">Ventas/Oportuniddes</a>
-                                    </li>
-                                    <li>
-                                        <a href="informesUser.php">Informes</a>
-                                    </li>
+                        </li>
 
-                                    <li>
-                                        <a href="renovacionesUser.php">Renovaciones/Renovaciones Anticipadas</a>
-                                    </li>
-                                </ul>
+                        <li class="dropdown notification-list">
+                            <a class="nav-link dropdown-toggle nav-user arrow-none me-0" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                                <span class="account-user-avatar">
+                                    <img src="assets/images/imagen-usuario.png" alt="user-image" class="rounded-circle">
+                                </span>
+                                <span>
+                                    <span class="account-user-name"><?php echo $user['first_name'] . ' ' . $user['last_name'] ?></span>
+                                    <span class="account-position"><?php echo $user['role'] ?></span>
+                                </span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated topbar-dropdown-menu profile-dropdown">
+                                <!-- item-->
+                                <div class=" dropdown-header noti-title">
+                                    <h6 class="text-overflow m-0">Welcome !</h6>
+                                </div>
+
+                                <!-- item-->
+                                <a href="logout.php" class="dropdown-item notify-item">
+                                    <i class="mdi mdi-logout me-1"></i>
+                                    <span>Logout</span>
+                                </a>
                             </div>
-                        </li>
-
-                        <li class="side-nav-title side-nav-item">Apps</li>
-
-                        <li class="side-nav-item">
-                            <a href="apps-chat.html" class="side-nav-link">
-                                <i class="uil-comments-alt"></i>
-                                <span> Chat (Promixamente) </span>
-                            </a>
-                        </li>
-
-                        <li class="side-nav-item">
-                            <a href="apps-file-manager.html" class="side-nav-link">
-                                <i class="uil-folder-plus"></i>
-                                <span> File Manager (Proximamente) </span>
-                            </a>
                         </li>
 
                     </ul>
-
-                    <div class="clearfix"></div>
-
+                    <button class="button-menu-mobile open-left">
+                        <i class="mdi mdi-menu"></i>
+                    </button>
                 </div>
-                <!-- Sidebar -left -->
-
-            </div>
-
-            <div class="content-page">
-    <div class="content">
-        <!-- Topbar Start -->
-        <div class="navbar-custom">
-            <ul class="list-unstyled topbar-menu float-end mb-0">
-                <li class="dropdown notification-list d-lg-none">
-                    <a class="nav-link dropdown-toggle arrow-none" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                        <i class="dripicons-search noti-icon"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-animated dropdown-lg p-0">
-                        <form class="p-3">
-                            <input type="text" class="form-control" placeholder="Buscar..." aria-label="Recipient's username">
-                        </form>
-                    </div>
-                </li>
-
-                <li class="dropdown notification-list">
-                    <a class="nav-link dropdown-toggle arrow-none" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                        <i class="dripicons-bell noti-icon"></i>
-                        <span class="noti-icon-badge"></span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated dropdown-lg">
-
-                        <!-- item-->
-                        <div class="dropdown-item noti-title">
-                            <h5 class="m-0">
-                                <span class="float-end">
-                                    <a href="javascript: void(0);" class="text-dark">
-                                        <small>Limpiar Todo</small>
-                                    </a>
-                                </span>Notificaciones
-                            </h5>
-                        </div>
-
-                        <div style="max-height: 230px;" data-simplebar="">
-                            <!-- item-->
-                            <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                <div class="notify-icon bg-primary">
-                                    <i class="mdi mdi-comment-account-outline"></i>
-                                </div>
-                                <p class="notify-details">
-                                    <small class="text-muted"></small>
-                                </p>
-                            </a>
-                        </div>
-
-                        <!-- All-->
-                        <a href="javascript:void(0);" class="dropdown-item text-center text-primary notify-item notify-all">
-                            Ver Todo
-                        </a>
-
-                    </div>
-                </li>
-
-                <li class="notification-list">
-                    <a class="nav-link end-bar-toggle" href="javascript: void(0);">
-                        <i class="dripicons-gear noti-icon"></i>
-                    </a>
-                </li>
-
-                <li class="dropdown notification-list">
-                    <a class="nav-link dropdown-toggle nav-user arrow-none me-0" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                        <span class="account-user-avatar">
-                            <img src="assets/images/imagen-usuario.png" alt="user-image" class="rounded-circle">
-                        </span>
-                        <span>
-                            <span class="account-user-name"><?php echo $user['first_name'] . ' ' . $user['last_name']?></span>
-                            <span class="account-position"><?php echo $user['role']?></span>
-                        </span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated topbar-dropdown-menu profile-dropdown">
-                        <!-- item-->
-                        <div class=" dropdown-header noti-title">
-                            <h6 class="text-overflow m-0">Welcome !</h6>
-                        </div>
-
-                        <!-- item-->
-                        <a href="logout.php" class="dropdown-item notify-item">
-                            <i class="mdi mdi-logout me-1"></i>
-                            <span>Logout</span>
-                        </a>
-                    </div>
-                </li>
-
-            </ul>
-            <button class="button-menu-mobile open-left">
-                <i class="mdi mdi-menu"></i>
-            </button>
-            <div class="app-search dropdown d-none d-lg-block">
-                <form>
-                    <div class="input-group">
-                        <input type="text" class="form-control dropdown-toggle" placeholder="Search..." id="top-search">
-                        <span class="mdi mdi-magnify search-icon"></span>
-                        <button class="input-group-text btn-primary" type="submit">Search</button>
-                    </div>
-                </form>
-
-                <div class="dropdown-menu dropdown-menu-animated dropdown-lg" id="search-dropdown">
-                    <!-- item-->
-                    <div class="dropdown-header noti-title">
-                        <h5 class="text-overflow mb-2">Found <span class="text-danger">17</span> results</h5>
-                    </div>
-
-                    <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                        <i class="uil-notes font-16 me-1"></i>
-                        <span>Analytics Report</span>
-                    </a>
-
-                    <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                        <i class="uil-life-ring font-16 me-1"></i>
-                        <span>How can I help you?</span>
-                    </a>
-
-                    <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                        <i class="uil-cog font-16 me-1"></i>
-                        <span>User profile settings</span>
-                    </a>
-
-                    <!-- item-->
-                    <div class="dropdown-header noti-title">
-                        <h6 class="text-overflow mb-2 text-uppercase">Users</h6>
-                    </div>
-
-                    <div class="notification-list">
-                        <!-- item-->
-                        <a href="javascript:void(0);" class="dropdown-item notify-item">
-                            <div class="d-flex">
-                                <img class="d-flex me-2 rounded-circle" src="assets/images/users/avatar-2.jpg" alt="Generic placeholder image" height="32">
-                                <div class="w-100">
-                                    <h5 class="m-0 font-14">Erwin Brown</h5>
-                                    <span class="font-12 mb-0">UI Designer</span>
-                                </div>
-                            </div>
-                        </a>
-
-                        <!-- item-->
-                        <a href="javascript:void(0);" class="dropdown-item notify-item">
-                            <div class="d-flex">
-                                <img class="d-flex me-2 rounded-circle" src="assets/images/users/avatar-5.jpg" alt="Generic placeholder image" height="32">
-                                <div class="w-100">
-                                    <h5 class="m-0 font-14">Jacob Deo</h5>
-                                    <span class="font-12 mb-0">Developer</span>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
